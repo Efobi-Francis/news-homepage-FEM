420e1f2501d514750282831995ef74ed97161e80
f0b <francisefobi@gmail.com>
2023-10-26 16:10:04 +0100

    text size

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -6,21 +6,21 @@ export default function Trending() {
         <h3 className=' text-[hsl(35,77%,62%)] text-3xl font-bold mb-10 font-inter-bold'>New</h3>
         <section>
             <p className=' text-[hsl(233,8%,79%)]'>
-                <h4 className=' text-[hsl(36,100%,99%)] text-xl mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>Hydrogen VS Electric Cars</a></h4>
+                <h4 className=' text-[hsl(36,100%,99%)] text-lg mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>Hydrogen VS Electric Cars</a></h4>
                 Will hydrogen-fueled cars ever catch up to EVs?
             </p>
 
             <div className=' h-[2px] w-full bg-[hsl(236,13%,42%)] my-5'></div>
 
             <p className=' text-[hsl(233,8%,79%)]'>
-                <h4 className=' text-[hsl(36,100%,99%)] text-xl mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>The Downsides of AI Artistry</a></h4>
+                <h4 className=' text-[hsl(36,100%,99%)] text-lg mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>The Downsides of AI Artistry</a></h4>
                 What are the possible adverse effects of on-demand AI image generation?
             </p>
 
             <div className=' h-[2px] w-full bg-[hsl(236,13%,42%)] my-5'></div>
 
             <p className=' text-[hsl(233,8%,79%)]'>
-                <h4 className=' text-[hsl(36,100%,99%)] text-xl mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>Is VC Funding Drying Up?</a></h4>
+                <h4 className=' text-[hsl(36,100%,99%)] text-lg mb-2 font-bold'><a href="" className=' hover:text-[hsl(35,77%,62%)]'>Is VC Funding Drying Up?</a></h4>
                 Private funding by VC firms is down 50% YOY. We take a look at what that means.
             </p>
         </section>
